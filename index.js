c6df8d9ea45809398e1e86f8b3cc1073c17953b3
Ryan Lee <ryanlee91@gmail.com>
2017-11-14 12:06:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const driver = {};
 function updateDriverWithKeyAndValue(driver,key,value){
   return Object.assign({},driver,{[key]: value});
 }
+
+function destructivelyUpdateDriverWithKeyAndValue(driver,key,value){
+  obj[driver] = value;
+  return obj;
+}
